36856372af51090563a89fcabaec3714822d72dc
alois-renggli <alois.renggli@gmail.com>
2018-08-17 14:08:24 -0500

    Refactored the javascript folder from JS to js

---
+++ js/coding-challenge-amateur.js
@@ -0,0 +1,51 @@
+"use strict";
+
+//1. Write a conditional statement to find the sign of the product
+// of three numbers.
+// a. Example: 5, -7, -2
+//     i. The output sign should be -
+// b. Example: -8, 4, 8
+//     i. The output sign should be +
+
+
+var a = 5;
+var b = -7;
+var c = -2;
+
+if ((a*b*c) >= 0 ){
+    console.log("+");
+}else{
+    console.log("-");
+}
+
+// 2. Write a conditional statement to sort
+// five numbers from the highest to lowest.
+//a. Example: 0, -1, 4, .6, -.5
+//  i. The output should be 4, 0.6, 0 , -1, -0.5
+
+var num = [];
+
+num.forEach(
+    function()
+
+)
+if (a > b && a > c && a >d && a > e){
+    console.log()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
